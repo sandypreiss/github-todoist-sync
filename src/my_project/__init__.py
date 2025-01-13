fdba516cff78928728b453f2e865fb9a7ecef085
Preiss, Sandy <apreiss@rti.org>
2025-01-13 16:27:28 -0500

    Repo setup

--- src/my_project/__init__.py
+++
@@ -1,6 +0,0 @@
-# __init__.py lets python know this is a module, and can also be used to declare
-# top-level imports
-
-from .core import cowsay_nice_template
-
-__all__ = ["cowsay_nice_template"]
--- src/my_project/core.py
+++
@@ -1,5 +0,0 @@
-import cowsay
-
-
-def cowsay_nice_template() -> str:
-    return cowsay.get_output_string("cow", "Nice Project Template!")
